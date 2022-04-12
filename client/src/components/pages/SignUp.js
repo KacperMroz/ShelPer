2a784b58c832940c7eb99d9443a443abe1bf05c2
Katarzyna Poreba <katarzynaporeba@icloud.com>
2022-04-12 10:28:42 +0200

    fixed path

--- client/src/components/pages/SignUp.js
+++ client/src/components/pages/SignUp.js
@@ -4,8 +4,8 @@ const SignUp = () => {
   return (
     <div>
       <h1>Wybierz typ swojego konta</h1>
-      <Button text={'Osoba prywatna'} navigate={'./sign-up-personal'} />
-      <Button text={'Schronisko'} navigate={'./sign-up-shelter'} />
+      <Button text={'Osoba prywatna'} navigate={'/sign-up-personal'} />
+      <Button text={'Schronisko'} navigate={'/sign-up-shelter'} />
     </div>
   );
 };
