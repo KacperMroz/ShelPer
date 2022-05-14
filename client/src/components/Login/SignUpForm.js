84ac50ef209138d2f4d1a9e58ed7a41baf6c909e
Kacper <kpawlak2000@gmail.com>
2022-05-14 19:58:06 +0200

    SignUpForm tweaks

--- client/src/components/Login/SignUpForm.js
+++ client/src/components/Login/SignUpForm.js
@@ -3,29 +3,68 @@ import { Button } from '../UI/Button';
 import { NavLink as Link } from 'react-router-dom';
 import './SignUpForm.css';
 
-export const SignUpForm = ({ text, navigate }) => {
+export const SignUpForm = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const [error, setError] = useState('');
 
-  const setUsernameHandler = event => {
+  const user = {
+    email,
+    password,
+    confirmPassword,
+  };
+
+  const emailHandler = event => {
     setEmail(event.target.value);
   };
-  const setPasswordHandler = event => {
+  const passwordHandler = event => {
     setPassword(event.target.value);
   };
-  const setConfirmPasswordHandler = event => {
+  const confirmPasswordHandler = event => {
     setConfirmPassword(event.target.value);
+  };
+
+  const validateEmail = email => {
+    // check for @
+    if (email.indexOf('@') === -1) {
+      return false;
+    }
+    // check for .
+    if (email.indexOf('.') === -1) {
+      return false;
+    }
+    return true;
+  };
+
+  const validatePassword = password => {
+    // check for length
+    if (password.length < 6) {
+      return false;
+    }
+    return true;
+  };
+
+  const validateForm = () => {
+    if (!validateEmail(email)) {
+      setError('Podaj poprawny adres e-mail.');
+      return false;
+    }
+    if (!validatePassword(password)) {
+      setError('Hasło musi mieć przynajmniej 6 znaków');
+      return false;
+    }
+    if (password !== confirmPassword) {
+      setError('Hasła nie są identyczne.');
+      return false;
+    }
+    return true;
   };
 
   const submitHandler = e => {
     e.preventDefault();
-    if (email === 'admin' && password === 'admin') {
-      setError('');
-      window.location.href = '/';
-    } else {
-      setError('Invalid email or password');
+    if (validateForm()) {
+      console.log(user);
     }
   };
 
@@ -36,34 +75,43 @@ export const SignUpForm = ({ text, navigate }) => {
         <div className='input-div'>
           <div>
             <input
-              className='log-sign-input'
+              className={'log-sign-input'}
               type='text'
               value={email}
-              onChange={setUsernameHandler}
-              placeholder={'Podaj swój e-mail'}
+              onChange={emailHandler}
+              placeholder='Podaj swój e-mail'
             />
           </div>
           <div>
             <input
-              className='log-sign-input'
+              className={'log-sign-input'}
               type='password'
               value={password}
-              onChange={setPasswordHandler}
-              placeholder={'Podaj swoje hasło'}
+              onChange={passwordHandler}
+              placeholder='Podaj swoje hasło'
             />
           </div>
           <div>
             <input
-              className='log-sign-input'
+              className={'log-sign-input'}
               type='password'
               value={confirmPassword}
-              onChange={setConfirmPasswordHandler}
-              placeholder={'Podaj swoje hasło'}
+              onChange={confirmPasswordHandler}
+              onBlur={validatePassword}
+              placeholder='Podaj ponownie swoje hasło'
             />
           </div>
         </div>
         <div>
-          <Button text={text} navigate={navigate} />
+          <button
+            className='log-sign-button'
+            type='submit'
+            onClick={submitHandler}
+          >
+            Zarejestruj się
+          </button>
+          {/* <Button text={text} navigate={navigate} />
+           */}
         </div>
         <div className='bottom-text'>
           Posiadasz konto? <Link to='/login'>Zaloguj się</Link>
