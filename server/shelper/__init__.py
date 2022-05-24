16314b5fc9577db5a628ffcac5f051b44e3a0a73
Krystian Pasinski <krystian.pasinski00@gmail.com>
2022-05-24 11:37:47 +0200

    modified CORS policy

--- server/shelper/__init__.py
+++ server/shelper/__init__.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    CORS(app)
+    CORS(app, supports_credentials=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'shelper.sqlite'),
