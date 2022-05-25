82f786d66c679e0d612e7cd8019d9222b071ac03
Katarzyna Poreba <katarzynaporeba@icloud.com>
2022-05-25 23:35:17 +0200

    Modified animal fetch

--- client/src/components/AddPost/Form.js
+++ client/src/components/AddPost/Form.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Type from '../AnimalSpec/Type';
 import Sex from '../AnimalSpec/Sex';
 import Color from '../AnimalSpec/Color';
@@ -6,38 +6,23 @@ import Size from '../AnimalSpec/Size';
 import Photo from '../AnimalSpec/Photo';
 
 const Form = () => {
-  const [type, setType] = React.useState('');
-  const [sex, setSex] = React.useState(false);
-  const [color, setColor] = React.useState('');
-  const [size, setSize] = React.useState('');
-  const [age, setAge] = React.useState('');
-  const [name, setName] = React.useState('');
-  const [weight, setWeight] = React.useState('');
-  const [description, setDescription] = React.useState('');
   const [photo, setPhoto] = React.useState([]);
-  const [healthy, setHealthy] = React.useState(true);
   const [error, setError] = React.useState('');
-  const formData = new FormData();
-
-  const handleInputChange = (e, set) => {
-    set(e.target.value);
-  };
+  const [model, setModel] = React.useState({
+      name: '',
+      age: '',
+      weight: '',
+      description: '',
+      healthy: true,
+      male: true,
+      color: '',
+      breed_id: 1,
+      size_id: 1,
+      animal_type_id: 1,
+  });
 
   const handleFileInput = e => {
     setPhoto([...photo, ...e.target.files]);
-  };
-
-  const formDataStatic = {
-    name: 'Michał',
-    age: '3',
-    weight: '5',
-    description: 'Lorem ipsum',
-    healthy: true,
-    male: false,
-    color: 'red',
-    breed_id: 5,
-    size_id: 2,
-    animal_type_id: 1,
   };
 
   const addAnimal = async () => {
@@ -47,7 +32,7 @@ const Form = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(formDataStatic),
+        body: JSON.stringify(model),
       });
       const data = await response.json();
       if (data.message) {
@@ -60,52 +45,42 @@ const Form = () => {
 
   const handleSubmit = e => {
     e.preventDefault();
-    formData.append('name', name);
-    formData.append('age', age);
-    formData.append('weight', weight);
-    formData.append('description', description);
-    formData.append('healthy', healthy);
-    formData.append('male', sex);
-    formData.append('color', color);
-    formData.append('breed_id', 1);
-    formData.append('size_id', 1);
-    formData.append('animal_type_id', 1);
-    console.log('Form submitted ' + formData.forEach(e => console.log(e)));
     addAnimal();
   };
 
   return (
     <form>
       <Type
-        handleTypeChange={e => handleInputChange(e, setType)}
+        handleTypeChange = {e => setModel({...model, type: e.target.value})}
         type='radio'
       />
-      <Sex handleSexChange={e => handleInputChange(e, setSex)} type='radio' />
+      <Sex handleSexChange = {e => setModel({...model, sex: e.target.value})}
+           type='radio' />
       <Color
-        handleColorChange={e => handleInputChange(e, setColor)}
+        handleColorChange = {e => setModel({...model, color: e.target.value})}
         type='radio'
       />
       <Size
-        handleSizeChange={e => handleInputChange(e, setSize)}
+        handleSizeChange = {e => setModel({...model, size_id: 2})}
         type='radio'
       />
       <input
         type='text'
-        onChange={e => setAge(e.target.value)}
+        onChange={e => setModel({...model, age: e.target.value})}
         placeholder={'Podaj wiek zwierzaka'}
       />
       <input
         type='text'
-        onChange={e => setName(e.target.value)}
+        onChange={e => setModel({...model, name: e.target.value})}
         placeholder={'Podaj imie zwierzaka'}
       />
       <input
         type='text'
-        onChange={e => setWeight(e.target.value)}
+        onChange={e => setModel({...model, weight: e.target.value})}
         placeholder={'Podaj wagę zwierzaka (kg)'}
       />
       <textarea
-        onChange={e => setDescription(e.target.value)}
+          onChange={e => setModel({...model, description: e.target.value})}
         placeholder={'Opis zwierzaka'}
       />
       <div>
