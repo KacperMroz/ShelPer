f78cd1985e6bff277736462fe2668bf4452e830a
pnkvch <pinkevychart@gmail.com>
2022-05-27 01:05:06 +0200

    fix: form issues

--- client/src/components/AnimalSpec/Input/index.js
+++ client/src/components/AnimalSpec/Input/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './index.css';
 
-const Input = ({ value, placeholder, handleChange }) => (
+const Input = ({ name, value, placeholder, handleChange }) => (
   <input
     className="input"
     type="text"
+    name={name}
     placeholder={placeholder}
     value={value}
     onChange={handleChange}
