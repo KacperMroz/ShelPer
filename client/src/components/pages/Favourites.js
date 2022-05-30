caaed375bcf5b18dd0a8a26b1022baab69a58974
Kaspyy <50270928+Kaspyy@users.noreply.github.com>
2022-05-30 19:13:10 +0200

    fix favourites

--- client/src/components/pages/Favourites.js
+++ client/src/components/pages/Favourites.js
@@ -1,21 +1,24 @@
-import React from 'react';
-import NavBar from "../NavBar";
-import Filter from "../Animals/Filter";
-import "./Animals.css";
-import FetchFavourites from "../Animals/FetchFavourites";
+import React, { useState } from 'react';
+import NavBar from '../NavBar';
+import Filter from '../Animals/Filter';
+import './Animals.css';
+import FetchFavourites from '../Animals/FetchFavourites';
 
 const Favourites = () => {
-    return (
-        <div className='animals-base-container'>
-            <NavBar />
-            <div className='animals-content-container'>
-                <Filter />
-                <div className={'container'}>
-                    <FetchFavourites url={'/favourites'}/>
-                </div>
-            </div>
+  const [filters, setFilters] = useState('');
+  const [order, setOrder] = useState('Najnowsze');
+
+  return (
+    <div className='animals-base-container'>
+      <NavBar />
+      <div className='animals-content-container'>
+        <Filter onSetFilters={setFilters} onSetOrder={setOrder} />
+        <div className={'container'}>
+          <FetchFavourites url={'/favourites'} />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Favourites;
